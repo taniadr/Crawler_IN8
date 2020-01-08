688611642bb2befeba88d6bc59e7b69e0871d321
Tania <diasrtania@gmail.com>
2020-01-08 18:23:58 -0300

    Update crawlerBeta.py

--- crawlerBeta.py
+++ crawlerBeta.py
@@ -6,17 +6,12 @@ from selenium.webdriver.chrome.options import Options
 import pandas as pd
 
 webdriver = "./chromedriver"
-
 driver = Chrome(webdriver)
-
 url = "https://g1.globo.com/"
-
 driver.get(url)
-
+timeout = 12
 items = driver.find_elements_by_class_name("feed-post")
-
 total = []
-
 count = 0
 
 for item in items:
@@ -29,5 +24,5 @@ for item in items:
     df = pd.DataFrame(total,columns=['quote_text','link'])
     count = count+1
     df.to_csv('feed.csv')
-    
+
 driver.close()
